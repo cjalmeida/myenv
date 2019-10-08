c6e76d74c26b3e895a49510b17fc53b09d33eefd
Cloves Almeida <almeida.cloves@bcg.com>
2019-10-08 15:24:59 +0000

    fix pip local dir

--- oh-my-bash.sh
+++ oh-my-bash.sh
@@ -2,6 +2,7 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 echo "alias ls='ls --color'" >> ~/.bashrc
 echo "alias vi=vim" >> ~/.bashrc
+echo 'export PATH="~/.local/bin:$PATH"' >> ~/.bashrc
 sed -i -E 's/OSH_THEME=".+?"/OSH_THEME="powerline-multiline"/g' ~/.bashrc
 THEME_FILE=~/.oh-my-bash/themes/powerline-multiline/powerline-multiline.theme.sh
 sed -i -E 's/CWD_THEME_PROMPT_COLOR=.*/CWD_THEME_PROMPT_COLOR=24/g' $THEME_FILE
