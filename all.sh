ecae43a920fb3693bd024c2d192392cec2d5330e
Cloves Almeida <almeida.cloves@bcg.com>
2020-04-21 01:07:46 +0000

    add /usr/local/bin to path

--- all.sh
+++ all.sh
@@ -4,8 +4,8 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 echo "alias ls='ls --color'" >> ~/.bashrc
 echo "alias vi=vim" >> ~/.bashrc
-echo 'export PATH="~/.local/bin:$PATH"' >> ~/.bashrc
-sed -i -E 's/OSH_THEME=".+?"/OSH_THEME="envy"/g' ~/.bashrc
+echo 'export PATH="~/.local/bin:/usr/local/bin:$PATH"' >> ~/.bashrc
+sed -i -E 's/OSH_THEME=".+?"/OSH_THEME="bobby-python"/g' ~/.bashrc
 THEME_FILE=~/.oh-my-bash/themes/powerline-multiline/powerline-multiline.theme.sh
 
 #!/bin/bash
