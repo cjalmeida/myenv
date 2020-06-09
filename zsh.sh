52a5fbcb6b89136bc10f926ba706f5cde9675d80
Cloves Almeida <almeida.cloves@bcg.com>
2020-06-09 14:22:18 -0300

    update zsh

--- zsh.sh
+++ zsh.sh
@@ -1,12 +1,36 @@
+#!/bin/bash
+
 set -e
 sudo apt install -y zsh
 sudo chsh -s /usr/bin/zsh $USER
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-sed -i -E 's/ZSH_THEME=".+?"/ZSH_THEME="spaceship"/g' ~/.zshrc
-sed -i -E 's/^plugins=\(.+?\)/plugins=(git kubectl tmux)/g' ~/.zshrc
+# install antigen
+curl -L git.io/antigen > ~/antigen.zsh
 
-echo 'export SPACESHIP_KUBECTL_SHOW=true' >> ~/.zshrc
+# setup .zshrc
+cat << EOF
+# Set up the prompt
+source ~/antigen.zsh
+
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle git
+antigen bundle heroku
+antigen bundle pip
+antigen bundle lein
+antigen bundle command-not-found
+
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Load the theme.
+antigen theme denysdovhan/spaceship-prompt
+
+# Tell Antigen that you're done.
+antigen apply
+
+export SPACESHIP_DOCKER_SHOW=false
+export SPACESHIP_KUBECTL_SHOW=true
+EOF
